5d5f006893278abaf673838c8c42726fe66dac6f
alaurenz <laurenzi@cs.washington.edu>
2010-08-19 12:05:10 -0400

    Added copyright

--- kohana/application/classes/model/gather.php
+++ kohana/application/classes/model/gather.php
@@ -1,4 +1,25 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+/*******************************************************************
+
+Copyright 2010, Adrian Laurenzi
+
+This file is part of ImpactProbe.
+
+ImpactProbe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+at your option) any later version.
+
+ImpactProbe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ImpactProbe. If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************/
+defined('SYSPATH') or die('No direct script access.');
 
 class Model_Gather extends Model {
     
